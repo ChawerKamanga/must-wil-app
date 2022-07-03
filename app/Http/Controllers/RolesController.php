4963bb967a7c4aeb26f9271e604b3655a8fb61d8
Chawer Kamanga <chawerkamanga@gmail.com>
2022-07-03 18:30:13 +0200

    Authenticated.vue

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class RolesController extends Controller
 {
@@ -13,7 +14,7 @@ class RolesController extends Controller
      */
     public function index()
     {
-        return 'am here';
+        return Inertia::render('Roles/Index');
     }
 
     /**
---
+++ resources/js/Pages/Roles/Index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="">
+    <h2 class="text-4xl">Roles</h2>
+  </div>
+</template>
+
+<script setup>
+</script>
