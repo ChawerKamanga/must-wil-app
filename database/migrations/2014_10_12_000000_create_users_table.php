72b791d34f9a06b36c62275aa0662da0b768062f
Chawer Kamanga <chawerkamanga@gmail.com>
2022-07-03 07:49:55 +0200

    updated users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('fullname');
+            $table->char('gender');
             $table->string('email')->unique();
+            $table->string('phone_number')->unique();
+            $table->foreignId('programme_id')->nullable()->constrained();
+            $table->foreignId('organization_id')->nullable()->constrained();
+            $table->foreignId('role_id')->constrained();
+            $table->string('reg_number')->nullable();
+            $table->unsignedTinyInteger('year_of_study')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
