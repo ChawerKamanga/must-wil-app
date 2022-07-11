07d5f672b6a9ce43f9fbd3b46ae808fe66eee0c4
Chawer Kamanga <chawerkamanga@gmail.com>
2022-07-11 14:04:36 +0200

    added link to academic supervisor.index

--- resources/js/Components/DashboardNav.vue
+++ resources/js/Components/DashboardNav.vue
@@ -117,17 +117,8 @@
         </div>
 
         <!-- Menu Item 5 -->
-        <div>
-          <a href="#" class="menu-item-link group inactive-nav-item">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="menu-icon text-gray-300 group-hover:text-darkBlue"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              stroke-width="2"
-            >
-              <path d="M12 14l9-5-9-5-9 5 9 5z" />
+        <InactiveNavItem routeName="academic-supervisors.index" itemName="Acad. Supervisors">
+          <path d="M12 14l9-5-9-5-9 5 9 5z" />
               <path
                 d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
               />
@@ -136,14 +127,7 @@
                 stroke-linejoin="round"
                 d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
               />
-            </svg>
-            <span
-              class="menu-text text-gray-300 text-xs group-hover:text-darkBlue"
-              >Acad. Supervisors</span
-            >
-          </a>
-        </div>
-
+        </InactiveNavItem>
         <!-- Menu Item 6 -->
         <InactiveNavItem routeName="programmes.index" itemName="Programmes">
           <path
