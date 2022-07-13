9ecb9df47e6a919b4d001cf35fc0635ab62d1985
Chawer Kamanga <chawerkamanga@gmail.com>
2022-07-13 20:37:14 +0200

    added regions & districts migrations

--- database/migrations/2022_07_13_183308_create_districts_table.php
+++ database/migrations/2022_07_13_183308_create_districts_table.php
@@ -13,10 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('organizations', function (Blueprint $table) {
+        Schema::create('districts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->foreignId('district_id')->constrained();
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('organizations');
+        Schema::dropIfExists('districts');
     }
 };
--- database/migrations/2022_07_13_183322_create_regions_table.php
+++ database/migrations/2022_07_13_183322_create_regions_table.php
@@ -13,10 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('organizations', function (Blueprint $table) {
+        Schema::create('regions', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('district_id')->constrained();
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('organizations');
+        Schema::dropIfExists('regions');
     }
 };
