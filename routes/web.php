490cf4d13a8a1973fdbf10a0ff7fee99739891f7
Chawer Kamanga <chawerkamanga@gmail.com>
2022-07-20 18:38:06 +0200

    updated web.php to arrange the routes alphabatically

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AcademicSupervisorsController;
 use App\Http\Controllers\AssessmentsController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\EvaluationsController;
 use App\Http\Controllers\IndustrialSupervisorController;
 use App\Http\Controllers\InternsController;
 use App\Http\Controllers\OrganizationController;
@@ -24,21 +25,25 @@ use Inertia\Inertia;
 */
 
 Route::middleware(['auth'])->group(function () {
-    Route::resource('roles', RolesController::class)->except(['edit', 'show']);
-    Route::get('roles/edit/{role:slug}', [RolesController::class, 'edit'])->name('roles.edit');
+    Route::resource('assessments', AssessmentsController::class)->except(['edit', 'show']);
+    Route::get('assessments/{assessments:slug}', [AssessmentsController::class, 'show'])->name('assessments.show');
+    Route::get('assessments/edit/{assessments:slug}', [AssessmentsController::class, 'edit'])->name('assessments.edit');
+    Route::resource('academic-supervisors', AcademicSupervisorsController::class)->except(['edit', 'show']);
+    Route::get('academic-supervisors/edit/{academic_supervisors:slug}', [AcademicSupervisorsController::class, 'edit'])->name('academic-supervisors.edit');
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('evaluations', EvaluationsController::class)->except(['edit', 'show']);
+    Route::get('evaluations/{evaluations:slug}', [EvaluationsController::class, 'show'])->name('evaluations.show');
+    Route::get('evaluations/edit/{evaluations:slug}', [EvaluationsController::class, 'edit'])->name('evaluations.edit');
+    Route::resource('industrial-supervisors', IndustrialSupervisorController::class)->except(['edit', 'show']);
+    Route::get('industrial-supervisors/edit/{industrial_supervisors:slug}', [IndustrialSupervisorController::class, 'edit'])->name('industrial-supervisors.edit');
+    Route::resource('interns', InternsController::class)->except(['edit', 'show']);
+    Route::get('interns/edit/{interns:slug}', [InternsController::class, 'edit'])->name('interns.edit');
     Route::resource('organizations', OrganizationController::class)->except(['edit', 'show']);
     Route::get('organizations/edit/{organization:slug}', [OrganizationController::class, 'edit'])->name('organizations.edit');
     Route::resource('programmes', ProgrammesController::class)->except(['edit', 'show']);
     Route::get('programmes/edit/{programme:code}', [ProgrammesController::class, 'edit'])->name('programmes.edit');
-    Route::resource('industrial-supervisors', IndustrialSupervisorController::class)->except(['edit', 'show']);
-    Route::get('industrial-supervisors/edit/{industrial_supervisors:slug}', [IndustrialSupervisorController::class, 'edit'])->name('industrial-supervisors.edit');
-    Route::resource('interns', InternsController::class)->except(['edit', 'show']);
-    Route::resource('assessments', AssessmentsController::class)->except(['edit', 'show']);
-    Route::get('assessments/{assessments:slug}', [AssessmentsController::class, 'show'])->name('assessments.show');
-    Route::get('interns/edit/{interns:slug}', [InternsController::class, 'edit'])->name('interns.edit');
-    Route::resource('academic-supervisors', AcademicSupervisorsController::class)->except(['edit', 'show']);
-    Route::get('academic-supervisors/edit/{academic_supervisors:slug}', [AcademicSupervisorsController::class, 'edit'])->name('academic-supervisors.edit');
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('roles', RolesController::class)->except(['edit', 'show']);
+    Route::get('roles/edit/{role:slug}', [RolesController::class, 'edit'])->name('roles.edit');
 });
 
 
