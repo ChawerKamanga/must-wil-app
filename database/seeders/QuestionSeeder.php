ac7cb35b7ab232f4d82faeb37004caaac60d2d12
Chawer Kamanga <chawerkamanga@gmail.com>
2022-07-23 21:08:25 +0200

    updated QuestionSeeder.php

--- database/seeders/QuestionSeeder.php
+++ database/seeders/QuestionSeeder.php
@@ -57,6 +57,51 @@ class QuestionSeeder extends Seeder
                 'updated_at' => Carbon::now(),
                 'created_at' => Carbon::now(),
             ],
+            // Industrial Supervisor Evaluation of Intern
+            [
+                'question' => 'Personal initiative
+                    a)Utilization of personal and workplace resources             
+                    b)Responsiveness to feedback             
+                    Initiative towards job responsibilities',
+                'slug' => 'personal-initiative',
+                'evaluation_id' => 2,
+                'total_marks' => 10,
+                'updated_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
+            ],
+            [
+                'question' => 'Interpersonal skills  
+                a)Pursuit and integration of input from multiple viewpoints
+                b)Collaboration with others in accomplishing tasks
+                Understanding of cultural difference',
+                'slug' => 'interpersonal-skills',
+                'evaluation_id' => 2,
+                'total_marks' => 10,
+                'updated_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
+            ],
+            [
+                'question' => 'Professionalism   
+                a)Quality of intern\'s work
+                b)Professional, ethical behavior in the workplace
+                Management of time (punctual, meets deadlines)',
+                'slug' => 'professionalism',
+                'evaluation_id' => 2,
+                'total_marks' => 10,
+                'updated_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
+            ],
+            [
+                'question' => 'Communications skills   
+                a)Verbal communication (formal & informal)
+                b)Written communication (formal & informal)
+                Communication with people from differing backgrounds',
+                'slug' => 'communication-skills',
+                'evaluation_id' => 2,
+                'total_marks' => 10,
+                'updated_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
+            ],
         ];
 
         Questions::insert($questions);
