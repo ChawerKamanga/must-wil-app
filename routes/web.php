2389155f81af9a27e6a6fb4984c476b4019ba55e
Chawer Kamanga <chawerkamanga@gmail.com>
2022-07-27 23:19:55 +0200

    updated web.php

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('industrial-supervisors/edit/{industrial_supervisors:slug}', [IndustrialSupervisorController::class, 'edit'])->name('industrial-supervisors.edit');
     Route::resource('interns', InternsController::class)->except(['edit', 'show']);
     Route::get('users/edit/{user:slug}', [InternsController::class, 'edit'])->name('interns.edit');
-    Route::get('users/{user:slug}', [InternsController::class, 'show'])->name('interns.show');
+    Route::get('interns/{user:slug}', [InternsController::class, 'show'])->name('interns.show');
     Route::resource('organizations', OrganizationController::class)->except(['edit', 'show']);
     Route::get('organizations/edit/{organization:slug}', [OrganizationController::class, 'edit'])->name('organizations.edit');
     Route::get('organizations/{organization:slug}', [OrganizationController::class, 'show'])->name('organizations.show');
