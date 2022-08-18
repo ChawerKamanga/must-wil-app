b171c47b0c15a5493275406e6e9bc54d424d9315
Chawer Kamanga <chawerkamanga@gmail.com>
2022-08-18 23:05:19 +0200

    added style for the register form

--- resources/js/Pages/Auth/StudentRegister.vue
+++ resources/js/Pages/Auth/StudentRegister.vue
@@ -4,30 +4,29 @@
      <div
         class="relative flex flex-col m-6 space-y-10 bg-white shadow-2xl  md:flex-row md:space-y-0 md:m-0"
       >
-      <div class="px-10 py-5 space-y-5 w-[800px]">
-
+      <div class="px-10 py-10 space-y-7 w-[800px]">
         <div class="flex flex-col space-y-2">
-          <h1 class="font-bold text-xl">Register as a Student</h1>
-          <h6 class="text-gray-700 text-sm">Step 1 of 3</h6>
+          <h1 class="font-bold text-2xl mb-3">Register as a Student</h1>
+          <h6 class="text-gray-700 text-sm font-bold">Step 1 of 3</h6>
         </div>
 
         <div class="flex flex-col space-y-2">
-          <label for="fullname" class="text-sm font-bold">Fullname</label>
-          <input type="text" id="fullname" />
+          <label for="fullname" class="form-label">Fullname</label>
+          <input type="text" id="fullname" placeholder="Enter your fullname" class="text-xs text-gray-700 py-4" />
         </div>
   
         <div class="flex flex-col space-y-2">
-          <label for="email">Email</label>
-          <input type="email" id="email" />
+          <label for="email" class="form-label">Email</label>
+          <input type="email" id="email" placeholder="Enter your school email" class="text-xs text-gray-700 py-4" />
         </div>
 
          <div class="flex flex-col space-y-2">
-          <label for="password">Password</label>
-          <input type="password" id="password" />
+          <label for="password" class="form-label">Password</label>
+          <input type="password" id="password" placeholder="Enter your password" class="text-xs text-gray-700 py-4"/>
         </div>
 
         <div>
-            <button class="bg-veryDarkBlue text-white p-4">Next Step</button>
+            <button class="bg-veryDarkBlue text-white px-4 py-3">Next Step</button>
         </div>
       </div>
     </div>
