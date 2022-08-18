87e9a28342589b17c9b34a6e3eb5648a0a0f5ad7
Chawer Kamanga <chawerkamanga@gmail.com>
2022-08-18 23:24:36 +0200

    updated ui for user register

--- resources/js/Pages/Auth/StudentRegister.vue
+++ resources/js/Pages/Auth/StudentRegister.vue
@@ -2,10 +2,18 @@
   <Head title="Student Register" />
     <div class="auth-wrapper">
      <div
-        class="relative flex flex-col m-6 space-y-10 bg-white shadow-2xl  md:flex-row md:space-y-0 md:m-0"
+        class="relative flex flex-col m-6 space-y-10   md:flex-row md:space-y-0 md:m-0"
       >
-      <div class="px-10 py-10 space-y-7 w-[800px]">
-        <div class="flex flex-col space-y-2">
+      <div class="min-w-[600px]">
+        <div class="flex justify-center items-center mb-5">
+            <div class="progress-round bg-gray-800">1</div>
+            <div class="w-48 h-1 bg-gray-100"></div>
+            <div class="progress-round bg-gray-500">2</div>
+            <div class="w-48 h-1 bg-gray-100"></div>
+            <div class="progress-round bg-gray-500">3</div>
+        </div>
+       <div class="space-y-7 bg-white px-10 py-10  shadow-2xl rounded-lg">
+         <div class="flex flex-col space-y-2">
           <h1 class="font-bold text-2xl mb-3">Register as a Student</h1>
           <h6 class="text-gray-700 text-sm font-bold">Step 1 of 3</h6>
         </div>
@@ -26,8 +34,9 @@
         </div>
 
         <div>
-            <button class="bg-veryDarkBlue text-white px-4 py-3">Next Step</button>
+            <button class="bg-veryDarkBlue text-white px-4 py-3 rounded">Next Step</button>
         </div>
+       </div>
       </div>
     </div>
     </div>
