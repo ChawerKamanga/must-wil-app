9de2421b387c298656f6f32a4be31c99cf6463c8
Chawer Kamanga <chawerkamanga@gmail.com>
2022-10-31 09:53:54 +0200

    added search api

--- app/Http/Controllers/InternsAPIController.php
+++ app/Http/Controllers/InternsAPIController.php
@@ -8,9 +8,20 @@ use Illuminate\Http\Request;
 
 class InternsAPIController extends Controller
 {
-    public function index($pgId)
+    public function index($pgId=1, $name=null)
     {
-        $interns = User::where([['role_id', 4], ['programme_id', $pgId]])->get();
+        if ($name) {
+            $interns = User::where([
+                ['role_id', 4], 
+                ['programme_id', $pgId],
+                ['name', 'like', '%' . $name. '%']
+            ])->get();
+        }else {
+            $interns = User::where([
+                ['role_id', 4], 
+                ['programme_id', $pgId],
+            ])->get();
+        }
 
         return InternResource::collection($interns);
     }
--- routes/api.php
+++ routes/api.php
@@ -21,8 +21,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
     Route::get('user', [UserController::class, 'index']);
     Route::get('organizations', [OrganizationApiController::class, 'index']);
-    // Route::get("interns?pg", [InternsAPIController::class, 'index']);
-    Route::get("interns/{pgId}", [InternsAPIController::class, 'index']);
+    Route::get("interns/{pgId?}/search/{name?}", [InternsAPIController::class, 'index']);
 
 });
 
