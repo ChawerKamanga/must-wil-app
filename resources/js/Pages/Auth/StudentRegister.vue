1f25eb573a9b9a463a92f5d11e25ec5ce268a288
Chawer Kamanga <chawerkamanga@gmail.com>
2023-02-05 08:01:03 +0200

    updated StudentRegister.vue

--- resources/js/Pages/Auth/StudentRegister.vue
+++ resources/js/Pages/Auth/StudentRegister.vue
@@ -1,15 +1,14 @@
 <template>
+
   <Head title="Student Register" />
   <div class="auth-wrapper">
-    <div
-      class="
+    <div class="
         relative
         flex flex-col
         m-6
         space-y-10
         md:flex-row md:space-y-0 md:m-0
-      "
-    >
+      ">
       <div class="min-w-[600px]">
         <div v-if="formStep == 1" class="flex justify-center items-center mb-5">
           <div class="progress-round bg-veryDarkBlue">1</div>
@@ -55,91 +54,50 @@
             <div class="space-y-5" v-if="formStep == 1">
               <div class="flex flex-col space-y-2">
                 <label for="fullname" class="form-label">Fullname</label>
-                <input
-                  type="text"
-                  id="fullname"
-                  placeholder="Enter your fullname"
-                  class="text-xs placeholder:placeholder:text-gray-700 py-4"
-                  v-model="form.name"
-                />
+                <input type="text" id="fullname" placeholder="Enter your fullname"
+                  class="text-xs placeholder:placeholder:text-gray-700 py-4" v-model="form.name" />
               </div>
 
               <div class="flex flex-col space-y-2">
                 <label for="email" class="form-label">Email</label>
-                <input
-                  type="email"
-                  id="email"
-                  placeholder="Enter your school email"
-                  class="text-xs placeholder:text-gray-700 py-4"
-                  v-model="form.email"
-                />
+                <input type="email" id="email" placeholder="Enter your school email"
+                  class="text-xs placeholder:text-gray-700 py-4" v-model="form.email" />
               </div>
 
               <div class="flex flex-col space-y-2">
-                <label for="personal-number" class="form-label"
-                  >Phonenumber</label
-                >
-                <input
-                  type="text"
-                  id="personal-number"
-                  placeholder="Enter your phonenumber"
-                  class="text-xs placeholder:text-gray-700 py-4"
-                  v-model="form.phone_number"
-                />
+                <label for="personal-number" class="form-label">Phonenumber</label>
+                <input type="text" id="personal-number" placeholder="Enter your phonenumber"
+                  class="text-xs placeholder:text-gray-700 py-4" v-model="form.phone_number" />
               </div>
 
               <div class="flex flex-col space-y-2">
-                <label for="personal-number" class="form-label"
-                  >Next of Kin #</label
-                >
-                <input
-                  type="text"
-                  id="personal-number"
-                  placeholder="Enter your next of kin phonenumber"
-                  class="text-xs placeholder:text-gray-700 py-4"
-                  v-model="form.next_of_kin"
-                />
+                <label for="personal-number" class="form-label">Next of Kin #</label>
+                <input type="text" id="personal-number" placeholder="Enter your next of kin phonenumber"
+                  class="text-xs placeholder:text-gray-700 py-4" v-model="form.next_of_kin" />
               </div>
             </div>
 
             <!-- Second step -->
             <div class="space-y-5" v-if="formStep == 2">
               <div class="flex flex-col space-y-2">
-                <label for="personal-number" class="form-label"
-                  >Registration Number</label
-                >
-                <input
-                  type="text"
-                  id="personal-number"
-                  placeholder="Enter your reg number"
-                  class="text-xs placeholder:text-gray-700 py-4"
-                  v-model="form.reg_number"
-                />
+                <label for="personal-number" class="form-label">Registration Number</label>
+                <input type="text" id="personal-number" placeholder="Enter your reg number"
+                  class="text-xs placeholder:text-gray-700 py-4" v-model="form.reg_number" />
               </div>
 
               <div class="flex flex-col space-y-2">
                 <label for="program" class="form-label">Program of Study</label>
                 <select v-model="form.program_of_study" id="program-select">
-                  <option
-                    v-for="programme in programmes.data"
-                    :key="programme.id"
-                    :value="programme.id"
-                  >
+                  <option v-for="programme in programmes.data" :key="programme.id" :value="programme.id">
                     {{ programme.name }}
                   </option>
                 </select>
               </div>
 
               <div class="flex flex-col">
-                <label for="profile-pic" class="form-label"
-                  >Profile Picture</label
-                >
-                <input
-                  type="file"
-                  class="file-style"
-                  @input="form.profile_pic = $event.target.files[0]"
-                  accept=".jpg, .png, .jpeg"
-                />
+                <label for="profile-pic" class="form-label">Profile Picture</label>
+                <input type="file" class="file-style" @input="form.profile_pic = $event.target.files[0]"
+                  accept=".jpg, .png, .jpeg" />
                 <img v-if="url" :src="url" class="w-full mt-4 h-80" />
               </div>
 
@@ -147,23 +105,12 @@
                 <label for="gender" class="form-label">Gender</label>
                 <div class="flex space-x-4">
                   <div class="space-x-2">
-                    <input
-                      type="radio"
-                      id="male"
-                      v-model="form.gender"
-                      value="M"
-                      checked
-                    />
+                    <input type="radio" id="male" v-model="form.gender" value="M" checked />
                     <label for="male">Male</label>
                   </div>
 
                   <div class="space-x-2">
-                    <input
-                      id="female"
-                      type="radio"
-                      v-model="form.gender"
-                      value="F"
-                    />
+                    <input id="female" type="radio" v-model="form.gender" value="F" />
                     <label for="female">Female</label>
                   </div>
                 </div>
@@ -175,11 +122,7 @@
               <div class="flex flex-col space-y-2">
                 <label for="personal-number" class="form-label">Location</label>
                 <select id="program-select" v-model="form.district">
-                  <option
-                    v-for="(district, index) in districts.data"
-                    :key="index"
-                    :value="district.id"
-                  >
+                  <option v-for="(district, index) in districts.data" :key="index" :value="district.id">
                     {{ district.name }}
                   </option>
                 </select>
@@ -187,32 +130,19 @@
 
               <div class="flex flex-col space-y-2">
                 <label for="password" class="form-label">Enter Password</label>
-                <input
-                  type="password"
-                  id="password"
-                  placeholder="Enter Password"
-                  class="text-xs placeholder:text-gray-700 py-4"
-                  v-model="form.password"
-                />
+                <input type="password" id="password" placeholder="Enter Password"
+                  class="text-xs placeholder:text-gray-700 py-4" v-model="form.password" />
               </div>
 
               <div class="flex flex-col space-y-2">
-                <label for="confirm-password" class="form-label"
-                  >Confirm Password</label
-                >
-                <input
-                  type="password"
-                  id="confirm-password"
-                  placeholder="Confirm Password"
-                  class="text-xs placeholder:text-gray-700 py-4"
-                  v-model="form.password_confirmation"
-                />
+                <label for="confirm-password" class="form-label">Confirm Password</label>
+                <input type="password" id="confirm-password" placeholder="Confirm Password"
+                  class="text-xs placeholder:text-gray-700 py-4" v-model="form.password_confirmation" />
               </div>
             </div>
 
             <div v-if="formStep == 1" class="flex justify-end">
-              <div
-                class="
+              <div class="
                   bg-veryDarkBlue
                   text-white
                   px-4
@@ -223,28 +153,18 @@
                   items-center
                   justify-between
                   w-24
-                "
-                @click="nextStep"
-              >
+                " @click="nextStep">
                 <span>Next</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fill-rule="evenodd"
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd"
                     d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  />
+                    clip-rule="evenodd" />
                 </svg>
               </div>
             </div>
 
             <div v-if="formStep == 2" class="flex justify-between">
-              <div
-                class="
+              <div class="
                   bg-veryDarkBlue
                   text-white
                   px-4
@@ -255,26 +175,16 @@
                   justify-between
                   w-24
                   cursor-pointer
-                "
-                @click="prevStep"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fill-rule="evenodd"
+                " @click="prevStep">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd"
                     d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
-                    clip-rule="evenodd"
-                  />
+                    clip-rule="evenodd" />
                 </svg>
                 <span>Prev</span>
               </div>
 
-              <div
-                class="
+              <div class="
                   bg-veryDarkBlue
                   text-white
                   px-4
@@ -285,28 +195,18 @@
                   items-center
                   justify-between
                   w-24
-                "
-                @click="lastStep"
-              >
+                " @click="lastStep">
                 <span>Next</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fill-rule="evenodd"
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd"
                     d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  />
+                    clip-rule="evenodd" />
                 </svg>
               </div>
             </div>
 
             <div v-if="formStep == 3" class="flex justify-between">
-              <div
-                class="
+              <div class="
                   bg-veryDarkBlue
                   text-white
                   px-4
@@ -317,29 +217,16 @@
                   justify-between
                   w-24
                   cursor-pointer
-                "
-                @click="prevStep"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fill-rule="evenodd"
+                " @click="prevStep">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd"
                     d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
-                    clip-rule="evenodd"
-                  />
+                    clip-rule="evenodd" />
                 </svg>
                 <span>Prev</span>
               </div>
 
-              <button
-                class="bg-veryDarkBlue text-white px-4 py-3 rounded"
-                type="submit"
-                @click="submit"
-              >
+              <button class="bg-veryDarkBlue text-white px-4 py-3 rounded" type="submit" @click="submit">
                 Submit
               </button>
             </div>
