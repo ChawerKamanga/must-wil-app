468d459ecf179f0afb48be32e39be4d41d8c861a
Chawer Kamanga <chawerkamanga@gmail.com>
2023-02-06 09:45:45 +0200

    updated APIAuhController.php

--- app/Http/Controllers/APIAuthController.php
+++ app/Http/Controllers/APIAuthController.php
@@ -6,27 +6,39 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class APIAuthController extends Controller
 {
 
-    public function login(Request $request) {
+    public function login(Request $request)
+    {
         $fields = $request->validate([
             'email' => 'required|string',
             'password' => 'required|string',
-            'device_name' => 'required',
+            'device_name' => 'required|string'
         ]);
 
         // Check email
         $user = User::where('email', $fields['email'])->first();
 
+
         // Check password
-        if(!$user || !Hash::check($fields['password'], $user->password)) {
-            throw ValidationException::withMessages([
-                'message' => ['The provided credentials are incorrect.'],
-            ]);
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Invalid Credentials'
+            ], 401);
         }
 
-        return $user->createToken($request->device_name)->plainTextToken;
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
     }
+
 }
