97014575b21a9b5391cf7d33e57f897e5863f1d8
Chawer Kamanga <chawerkamanga@gmail.com>
2023-03-06 10:00:35 +0200

    Created PresentationScores.php

---
+++ app/Models/PresentationScores.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PresentationScores extends Model
+{
+    use HasFactory;
+}
