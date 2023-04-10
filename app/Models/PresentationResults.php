60da8caff8f37fa530647a77892b828ab3ef02b1
Chawer Kamanga <chawerkamanga@gmail.com>
2023-04-10 19:48:26 +0200

    updated create_presentation migration

---
+++ app/Models/PresentationResults.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PresentationResults extends Model
+{
+    use HasFactory;
+}
