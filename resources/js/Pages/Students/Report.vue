87dfd78981896c768919504bca8d1612b866ba70
Chawer Kamanga <chawerkamanga@gmail.com>
2023-04-25 19:54:55 +0200

    updated Report.vue

--- resources/js/Pages/Students/Report.vue
+++ resources/js/Pages/Students/Report.vue
@@ -60,7 +60,28 @@
                     ">
                     <span>Report Template Not Available</span>
                   </span>
-                  <button @click="showForm = !showForm">Toggle Form</button>
+                  <button @click="showForm = !showForm"
+                    class="text-white px-6 py-2 rounded-full border border-white flex space-x-4 justify-center items-center">
+                    <svg v-if="showForm" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                      class="w-6 h-6 text-white">
+                      <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+                      <path fill-rule="evenodd"
+                        d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z"
+                        clip-rule="evenodd" />
+                    </svg>
+
+                    <svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                      class="w-6 h-6">
+                      <path fill-rule="evenodd"
+                        d="M5.625 1.5H9a3.75 3.75 0 013.75 3.75v1.875c0 1.036.84 1.875 1.875 1.875H16.5a3.75 3.75 0 013.75 3.75v7.875c0 1.035-.84 1.875-1.875 1.875H5.625a1.875 1.875 0 01-1.875-1.875V3.375c0-1.036.84-1.875 1.875-1.875zm6.905 9.97a.75.75 0 00-1.06 0l-3 3a.75.75 0 101.06 1.06l1.72-1.72V18a.75.75 0 001.5 0v-4.19l1.72 1.72a.75.75 0 101.06-1.06l-3-3z"
+                        clip-rule="evenodd" />
+                      <path
+                        d="M14.25 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0016.5 7.5h-1.875a.375.375 0 01-.375-.375V5.25z" />
+                    </svg>
+
+
+
+                    <span> {{ showForm ? 'View Your Submitted Report' : 'Submit a Report' }}</span> </button>
                 </div>
               </div>
               <!-- Doc img -->
@@ -192,6 +213,11 @@ export default {
         form.post(route("interns.report.store"));
       }, 3000);
     }
+
+    if (authUser.value.report_url === "") {
+      this.showForm = false;
+    }
+
 
     return { authUser, submit, form };
   },
